49c8f2fbf27f8221f8f372063937d9b07608ccd7
APSchuurman <a.p.schuurman@gmail.com>
2016-02-21 15:21:21 +0100

    Created data access layer

---
+++ BrewzDomainDataAccessLayer/BrewerRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrewzDomain.Classes;
+using BrewzDomain.DataLayer;
+using System.Data.Entity;
+using BrewzRestfullWebService.Controllers;
+
+namespace BrewzDomainDataAccessLayer
+{
+    public class BrewerRepository : IBrewerRepository
+    {
+        private BrewzContext db = new BrewzContext();
+        private static List<Brewer> brewers;
+
+        public BrewerRepository()
+        {
+        }
+        public Brewer GetBrewerById(int id)
+        {
+            return db.brewers.Find(id);
+        }
+
+        public List<Brewer> GetBrewers()
+        {
+            DbSet<Brewer> brewersDbSet = db.brewers;
+            List<Brewer> brewersList = brewersDbSet.ToList();
+            if (brewersList.Count == 0)
+            {
+                InitializeBrewersController initialize = new InitializeBrewersController();
+                initialize.GetInitialzeBrewers();
+                brewersList = brewersDbSet.ToList();
+            }
+            return brewersList;
+        }
+    }
+}
---
+++ BrewzDomainDataAccessLayer/IBrewerRepository.cs
@@ -0,0 +1,15 @@
+﻿using BrewzDomain.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrewzDomainDataAccessLayer
+{
+    public interface IBrewerRepository
+    {
+        Brewer GetBrewerById(int id);
+        List<Brewer> GetBrewers();
+    }
+}
