e27ecb13afcdee19a7508521003f0b24d0776154
APSchuurman <a.p.schuurman@gmail.com>
2016-02-21 15:22:48 +0100

    Moved and updated views

---
+++ BrewzWPF/View/BrewerDetailView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BrewzWPF.Services;
+using System.Windows;
+
+namespace BrewzWPF
+{
+
+    public partial class DetailWindow : Window
+    {
+        BrewerDataService brewersService = new BrewerDataService();
+        int selectedId = 1;
+        public DetailWindow()
+        {
+            InitializeComponent();
+            this.Loaded += DetailWindow_Loaded;
+        }
+
+        void DetailWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = brewersService.GetBrewer(selectedId) ;
+        }
+    }
+}
---
+++ BrewzWPF/View/BrewerOverviewView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace BrewzWPF
+{
+
+    public partial class BrewerOverviewView : Window
+    {
+
+        public BrewerOverviewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
