7576b35ee7c0e832e32f62340c7f8ccb420e5438
APSchuurman <a.p.schuurman@gmail.com>
2016-02-21 15:24:38 +0100

    Added DataServices

---
+++ BrewzWPF/Services/BrewerDataService.cs
@@ -0,0 +1,26 @@
+﻿using BrewzDomain.Classes;
+using BrewzDomainDataAccessLayer;
+using System.Collections.Generic;
+
+namespace BrewzWPF.Services
+{
+    class BrewerDataService : IDataService
+    {
+        IBrewerRepository repository = new BrewerRepository();
+
+        public BrewerDataService()
+        {
+            this.repository = repository;
+        }
+
+        public List<Brewer> GetAllBrewers()
+        {
+            return repository.GetBrewers();
+        }
+
+        public Brewer GetBrewer(int id)
+        {
+            return repository.GetBrewerById(id);
+        }
+    }
+}
---
+++ BrewzWPF/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using BrewzDomain.Classes;
+using System.Collections.Generic;
+
+namespace BrewzWPF.Services
+{
+    public interface IDataService
+    {
+        Brewer GetBrewer(int brewerId);
+
+        List<Brewer> GetAllBrewers();
+
+    }
+}
