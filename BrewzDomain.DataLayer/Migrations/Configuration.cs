722f265b719dccdfe7f3332c035e93c5212a01ac
APSchuurman <a.p.schuurman@gmail.com>
2016-02-28 19:25:57 +0100

    Moved initialize data to migrations seed method.

--- BrewzDomain.DataLayer/Migrations/Configuration.cs
+++ BrewzDomain.DataLayer/Migrations/Configuration.cs
@@ -1,31 +1,77 @@
 namespace BrewzDomain.DataLayer.Migrations
 {
-    using System;
-    using System.Data.Entity;
+    using Classes;
+    using JsonClasses;
+    using Services;
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BrewzDomain.DataLayer.BrewzContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(BrewzDomain.DataLayer.BrewzContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            JsonDataService dataService = new JsonDataService();
+            List<BrewerJson> brewerJsonList = dataService.InitializeBrewers();
+            List<Brewer> brewerList = new List<Brewer>();
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // Map each BrewerJson to Brewer
+            foreach (var brewerJson in brewerJsonList)
+            {
+                Brewer brewer = MapBrewer(brewerJson);
+                brewer.Address = MapAddress(brewerJson);
+                brewer.Communication = MapCommunication(brewerJson);
+                brewerList.Add(brewer);
+            }
+            brewerList.ForEach(b => context.Brewers.AddOrUpdate(p => p.Name, b));
+            context.SaveChanges();
+        }
+
+        private Brewer MapBrewer(BrewerJson brewerJson)
+        {
+            Brewer brewer = new Brewer();
+            brewer.Name = brewerJson.Name;
+            brewer.DescriptionNl = brewerJson.DescriptionNl;
+            brewer.DescriptionEn = brewerJson.DescriptionEn;
+            brewer.OpeningTimesNl = brewerJson.OpeningTimesNl;
+            brewer.OpeningTimesEn = brewerJson.OpeningTimesEn;
+            brewer.IndividualsJoinGroupsNl = brewerJson.IndividualsJoinGroupsNl;
+            brewer.IndividualsJoinGroupsEn = brewerJson.IndividualsJoinGroupsEn;
+            brewer.ImagesUrl = brewerJson.ImagesUrl;
+            brewer.VideoUrl = brewerJson.VideoUrl;
+            brewer.TopFermentation = brewerJson.TopFermentation;
+            brewer.BottomFermentation = brewerJson.BottomFermentation;
+            brewer.SpontaneousFermentation = brewerJson.SpontaneousFermentation;
+            brewer.MixedFermentation = brewerJson.MixedFermentation;
+            brewer.DescriptionNl = brewerJson.DescriptionNl;
+            brewer.DescriptionEn = brewerJson.DescriptionEn;
+            return brewer;
+        }
+
+        private Address MapAddress(BrewerJson brewerJson)
+        {
+            Address address = new Address();
+            address.Street = brewerJson.Street;
+            address.HouseNumber = brewerJson.HouseNumber;
+            address.City = brewerJson.CityName;
+            address.Province = brewerJson.Province;
+            address.PostalCode = brewerJson.PostalCode;
+            return address;
+        }
+
+        private Communications MapCommunication(BrewerJson brewerJson)
+        {
+            Communications communication = new Communications();
+            communication.Email = brewerJson.Email;
+            communication.Phone = brewerJson.Phone;
+            communication.Mobile = brewerJson.Mobile;
+            communication.Fax = brewerJson.Fax;
+            communication.Website = brewerJson.Website;
+            return communication;
         }
     }
 }
