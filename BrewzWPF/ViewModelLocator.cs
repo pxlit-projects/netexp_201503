f685c5293a1330c44c7f133d29e68e65194b7719
APSchuurman <a.p.schuurman@gmail.com>
2016-03-06 11:56:53 +0100

    Added ViewModelLocator and updated views to use it.

---
+++ BrewzWPF/ViewModelLocator.cs
@@ -0,0 +1,32 @@
+﻿using BrewzWPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrewzWPF
+{
+    public class ViewModelLocator
+    {
+        private static BrewerOverviewViewModel brewerOverviewViewModel = new BrewerOverviewViewModel();
+        private static BrewerDetailViewModel brewerDetailViewModel = new BrewerDetailViewModel();
+
+        public static BrewerOverviewViewModel BrewerOverviewViewModel
+        {
+            get
+            {
+                return brewerOverviewViewModel;
+            }
+        }
+
+        public static BrewerDetailViewModel BrewerDetailViewModel
+        {
+            get
+            {
+                return brewerDetailViewModel;
+            }
+        }
+
+    }
+}
