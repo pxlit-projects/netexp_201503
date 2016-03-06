9a1f938a1dfbbf2e52841920c3fc089a169a447d
APSchuurman <a.p.schuurman@gmail.com>
2016-03-06 11:53:43 +0100

    Created BrewerDetailViewModel and updated BrewerDetailView.

--- BrewzWPF/View/BrewerDetailView.xaml.cs
+++ BrewzWPF/View/BrewerDetailView.xaml.cs
@@ -4,19 +4,12 @@ using System.Windows;
 namespace BrewzWPF
 {
 
-    public partial class DetailWindow : Window
+    public partial class BrewerDetailWindow : Window
     {
-        BrewerDataService brewersService = new BrewerDataService();
-        int selectedId = 1;
-        public DetailWindow()
+       public BrewerDetailWindow()
         {
             InitializeComponent();
-            this.Loaded += DetailWindow_Loaded;
         }
 
-        void DetailWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            this.DataContext = brewersService.GetBrewer(selectedId) ;
-        }
     }
 }
---
+++ BrewzWPF/ViewModel/BrewerDetailViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BrewzDomain.Classes;
+using System.ComponentModel;
+
+namespace BrewzWPF.ViewModel
+{
+    public class BrewerDetailViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Brewer selectedBrewer;
+        public Brewer SelectedBrewer
+        {
+            get
+            {
+                return selectedBrewer;
+            }
+            set
+            {
+                selectedBrewer = value;
+                RaisePropertyChanged("SelectedBrewer");
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
