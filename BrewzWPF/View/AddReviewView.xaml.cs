6974736cdd2b0c0f6ab415df28d0b1b9f09b0570
APSchuurman <a.p.schuurman@gmail.com>
2016-03-07 20:20:46 +0100

    Added AddReviewView.

---
+++ BrewzWPF/View/AddReviewView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace BrewzWPF.View
+{
+
+    public partial class AddReviewView : Window
+    {
+        public AddReviewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
